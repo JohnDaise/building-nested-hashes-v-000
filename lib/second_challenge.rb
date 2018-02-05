26552ddda021c5ebed7ac72dc959c4c4d4c926ce
John Daise <jdaise@gmail.com>
2018-02-05 03:44:01 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", },
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
       },
     :capulet => {
